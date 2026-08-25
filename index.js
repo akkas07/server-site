198ab3b283c7424b08183042b218af5e8e022ba9
Akkas Islam <akkasislam05@gmail.com>
2026-08-25 12:55:27 +0600

    feat: create /cars POST route to insert car data into MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv")
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 // mongodeb
@@ -21,14 +26,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const db = client.db("drivefleet");
+    const carsCollection = db.collection("cars");
+
+    // get all cars
+    app.post("/cars", async (req, res) => {
+      const carData = req.body;
+      console.log(carData);
+      const result = await carsCollection.insertOne(carData);
+      res.send(result);
+    });
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -36,5 +54,5 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("server is running fine");
+  res.send("server is running fine");
 });
